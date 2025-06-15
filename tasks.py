032f40251360761beee0ab1a08aadb375eb81893
swawiye <sovereign.wawiye@gmail.com>
2025-06-15 22:03:59 +0300

    sqlite table

--- tasks.py
+++ tasks.py
@@ -1,2 +1,16 @@
 import sqlite3
 
+connection = sqlite3.connect('Tasks.db') # connect to SQLite and create a database file
+
+# Create a cursor object
+cursor = connection.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        title TEXT, 
+        description TEXT, 
+        due_date DATE, 
+        completed TEXT
+    )  
+''')
