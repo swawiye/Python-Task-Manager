1e04d73c7e45be11be084f41ce1f54f3e118070b
swawiye <sovereign.wawiye@gmail.com>
2025-06-15 22:29:50 +0300

    database operations

--- tasks.py
+++ tasks.py
@@ -8,9 +8,38 @@ cursor = connection.cursor()
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        title TEXT, 
+        title TEXT NOT NULL, 
         description TEXT, 
-        due_date DATE, 
+        due_date TEXT, 
         completed TEXT
     )  
 ''')
+
+# Inserting data
+tasks_data = [
+    ('Making dinner', 'Roast potatoes and grilled salmon', 15/6/2025, 'YES'),
+    ('Doing laundry', 'White clothes', 15/6/2025, 'NO')
+]
+
+cursor.executemany('''
+    INSERT INTO tasks (title, description, due_date, completed)
+    VALUES(?, ?, ?, ?)
+''', tasks_data)
+
+# Read data
+print("\n--- Tasks ---")
+cursor.execute('SELECT * FROM tasks')
+for row in cursor.fetchall():
+    print(row)
+
+# Updating data
+cursor.execute('''
+    UPDATE tasks SET title = ? WHERE id = ?                 
+''', ('Washing clothes', 4))
+
+# Deleting data
+cursor.execute("DELETE FROM tasks WHERE completed = NO")
+
+# Saving changes and Close the connection to the database (only once )
+connection.commit()
+connection.close() 
